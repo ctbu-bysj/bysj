e85d1ead15ae514f5b5d12f1d44a0799156488e7
You <You@DESKTOP-QBEULMR>
2019-05-19 14:21:20 +0800

    环境搭建

---
+++ src/main/java/com/ctbu/schoolofai/btsjmanager/BtsjmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.ctbu.schoolofai.btsjmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtsjmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtsjmanagerApplication.class, args);
+    }
+
+}

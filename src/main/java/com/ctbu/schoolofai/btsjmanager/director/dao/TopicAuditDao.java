93462961e10ea87d2535bae4e1900150c0f2c0b4
rain <569492979@qq.com>
2019-05-25 13:58:44 +0800

    负责人包清除错误信息-----饶航

--- src/main/java/com/ctbu/schoolofai/btsjmanager/director/dao/TopicAuditDao.java
+++ src/main/java/com/ctbu/schoolofai/btsjmanager/director/dao/TopicAuditDao.java
@@ -1,8 +1,17 @@
 package com.ctbu.schoolofai.btsjmanager.director.dao;
 
+import com.ctbu.schoolofai.btsjmanager.publicTable.domain.Student;
 import com.ctbu.schoolofai.btsjmanager.publicTable.domain.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
+/**
+ * 出题审核
+ */
 public interface TopicAuditDao extends JpaRepository<Topic,String> {
+    @Query(value = "select * from topic where state='state'",nativeQuery = true)//书写查询语句
+    public List<Topic> findByState(Short state);
 
 }
